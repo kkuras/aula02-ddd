71670b6f5643fe4878b3bb80c307cf5ff50d4a7a
kkura <flkuratomi@gmail.com>
2026-03-06 09:53:56 -0300

    aula 2

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PrimeiroPrograma {
+
+        public static void main(String[] args) {
+             // leitura de dados Part1
+            int idade, qFilhos;
+            boolean casado;
+            String nome;
+            Scanner leitor = new Scanner(System.in);
+            System.out.println("Hello World!: ");
+            nome = leitor.nextLine();
+            System.out.println("Bem vindo ");
+            idade = leitor.nextInt();
+            System.out.println("Bem vindo " + nome);
+
+
+        }
+
+
+
+}
